e96f63ccca6f033c4d695a8a073c8da7b8a232af
Maria DelSignore <maria.ad27@gmail.com>
2021-10-02 21:40:12 -0600

    Update article import count

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,4 +1,4 @@
 export const getNews = () => {
-  return fetch('https://gnews.io/api/v4/search?q=example&token=461ae41c507a8d0eece2d688d1af4369&lang=en&max=10')
+  return fetch('https://gnews.io/api/v4/search?q=example&max=10&token=461ae41c507a8d0eece2d688d1af4369&lang=en')
     .then(res => res.json())
 }
