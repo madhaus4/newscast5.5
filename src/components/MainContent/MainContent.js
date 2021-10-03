71334ad1831fdac4eaee34a881272b4947656815
Maria DelSignore <maria.ad27@gmail.com>
2021-10-02 21:29:41 -0600

    Build fetch call

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,7 +1,22 @@
-import './MainContent.css';
+import { set } from 'cypress/types/lodash';
+import React, { useState, useEffect } from 'react';
+
 import { getNews } from '../../utils/apiCalls';
+import './MainContent.css';
 
 const MainContent = () => {
+  const [allNews, setAllNews] = useState([])
+
+  const fetchNews = () => {
+    getNews()
+      .then(data => setAllNews(data))
+      .catch(error => console.log('ERRR: ', error))
+  }
+
+  useEffect(() => {
+    fetchNews()
+  }, [])
+
 
   return (
     <h2>dis main content</h2>
