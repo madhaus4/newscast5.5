8ea35217a5554c81903fafc03890ba2b1a4f7974
Maria DelSignore <maria.ad27@gmail.com>
2021-10-02 21:22:28 -0600

    build component structure

---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,12 @@
+import './MainContent.css';
+import { getNews } from '../../utils/apiCalls';
+
+const MainContent = () => {
+
+  return (
+    <h2>dis main content</h2>
+  )
+}
+
+
+export default MainContent;
