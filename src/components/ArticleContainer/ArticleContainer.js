80024961beb9374cabfbace0dc0bb956224328be
Maria DelSignore <maria.ad27@gmail.com>
2021-10-02 21:55:34 -0600

    Map through news data

--- src/components/ArticleContainer/ArticleContainer.js
+++ src/components/ArticleContainer/ArticleContainer.js
@@ -1,10 +1,15 @@
+import ArticleCard from '../ArticleCard/ArticleCard';
 import './ArticleContainer.css';
 
-const ArticleContainer = () => {
+const ArticleContainer = ({ allNews }) => {
+  const newsArticles = allNews.map(element => {
+    return <ArticleCard key={element.publishedAt}/>
+
+  })
 
   return (
     <div>
-      <h3>hallo</h3>
+      {newsArticles}
     </div>
   )
 }
