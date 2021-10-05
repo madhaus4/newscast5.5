8a4d9e2938479f5676410b58841b91bdcbf1820e
Alamin <alaminshaheen@iut-dhaka.edu>
2021-10-05 16:19:28 +0600

    added comments for other users to understand

--- src/utils/getUniqueArticles.js
+++ src/utils/getUniqueArticles.js
@@ -1,9 +1,16 @@
 export const uniqueArticles = (articles) => {
+  // creating map data structure to store unique article titles
   const uniqueArticleTitles = new Map();
+  // result array for returning unique articles
   let result = [];
+  // iterating over every articles
   articles.forEach((article) => {
+    // checking if map contains article title already
     if (!uniqueArticleTitles.has(article.title)) {
+      // if it does not contain article title that means this is a new article
+      // adding this new article title to map so that if a duplicate article with same title is found it will be ignored
       uniqueArticleTitles.set(article.title);
+      // pushing the article to result array
       result.push(article);
     }
   });
