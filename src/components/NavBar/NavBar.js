82e13450776fd89abeee6b91d6272a19ea4fd113
Maria DelSignore <maria.ad27@gmail.com>
2021-10-09 22:17:20 -0600

    Add topic buttons

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,17 @@ const NavBar = () => {
   return (
     <section className='nav-container'>
       <h2>Topics</h2>
+      <div className='topic-btn-container'>
+        <button>Breaking News</button>
+        <button>World</button>
+        <button>Nation</button>
+        <button>Business</button>
+        <button>Technology</button>
+        <button>Entertainment</button>
+        <button>Sports</button>
+        <button>Science</button>
+        <button>Health</button>
+      </div>
     </section>
   )
 }
