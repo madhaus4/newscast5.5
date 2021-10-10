c021218a1507d45a65ed8797c41769a8dbaaf1a8
Maria DelSignore <maria.ad27@gmail.com>
2021-10-10 00:12:03 -0600

    Import icons

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,3 +1,7 @@
+import removeFrom from '../../images/removeFrom.png';
+import addTo from '../../images/addTo.png';
+import favorite from '../../images/favorite.png';
+import unfavorite from '../../images/unfavorite.png';
 import './ArticleCard.css';
 
 const ArticleCard = ({ content, description, title, image, publishedAt, url, sourceName, sourceUrl }) => {
@@ -7,6 +11,8 @@ const ArticleCard = ({ content, description, title, image, publishedAt, url, sou
       <div className='article-info'>
         <h3>{title}</h3>
         <p>{description}</p>
+        <img className='reading-list-icon' src={addTo} alt='' />
+        <img className='favorite-icon' src={unfavorite} alt='' />
       </div>
     </article>
   )
