49e2b194ee2f397e79e87939b8bc21e6d815744f
Maria DelSignore <maria.ad27@gmail.com>
2021-10-10 18:11:03 -0600

    Add env file for api key

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,4 +1,11 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const getNews = () => {
-  return fetch('https://gnews.io/api/v4/search?q=example&max=10&token=461ae41c507a8d0eece2d688d1af4369&lang=en')
+  return fetch(`https://gnews.io/api/v4/search?q=example&max=10&token=${API_KEY}&lang=en`)
     .then(res => res.json())
 }
+
+// export const getHeadlineNews = () => {
+//   return fetch(`https://gnews.io/api/v4/top-headline?q=example&max=10&token=${API_KEY}&lang=en&topic=${topic}`)
+//     .then(res => res.json())
+// }
